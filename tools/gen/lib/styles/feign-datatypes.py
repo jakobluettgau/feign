6eda2ebcf35c373e9dc581faa108c2b5bd5544f6
Jakob Luettgau <jakob@whatz.eu>
2014-07-02 17:11:48 +0200

    gen: add feign datatype style

---
+++ tools/gen/lib/styles/feign-datatypes.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+
+import skeletonBuilder
+
+class Style(skeletonBuilder.Writer):
+    #
+    # @brief Write a source file
+    #
+    # @param functions A list of function-objects to write
+    def writeOutput(self, options, functionList, templateParameters, precompiler):
+        # open the output file for writing
+        output = open(options.outputFile, 'w')
+
+        # write all needed includes
+        for match in templateParameters["includes"]:
+            print('#include ', match, end='\n', file=output)
+        print('\n', file=output)
+
+        # write all global-Templates
+        for function in functionList:
+            functionVariables = self.functionVariables(function)
+
+            for templ in function.usedTemplateList:
+                if templ.output('global', functionVariables) != '':
+                    print(templ.output('global', functionVariables), file=output)
+        print("", file=output)
+
+        # write global once from template
+        print(templateParameters["globalOnce"], file=output)
+
+        # write all functions-bodies
+        for function in functionList:
+            functionVariables = self.functionVariables(function)
+
+            # write function signature
+
+            print("struct", end=' ', sep='', file=output)
+            print("posix_", function.name, "_struct", end='\n', sep='', file=output)
+            print("{", end='\n', sep='', file=output)
+
+            # a variable to save the return-value
+            returnType = function.type
+
+            if returnType != "void":
+                print('\t', returnType, ' ret;', end='\n', sep='',
+                      file=output)
+                
+            print('};', end='\n', file=output)
+
+            #e.g.  typedef struct posix_open_struct posix_open_data;
+
+            print("typedef struct", end=' ', sep='', file=output)
+            print("posix_", function.name, "_struct", end=' ', sep='', file=output) 
+            print("posix_", function.name, "_data", end=';\n\n\n', sep='', file=output) 
+
+
+        # close the file
+        output.close()
