ede5180e5773a05f71b5366c6995ac35159f99cf
Jakob Luettgau <jakob@whatz.eu>
2014-07-15 17:54:58 +0200

    move sample plugin family, streamline posix plugin generation

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
 #Flexible Event Imitation Engine
 
-
 Feign is a modular tool to recreate system activity for benchmarking, stress testing
-and debugging with targeting parallel workloads.
+and debugging targeting parallel workloads.
+
 
 ## Building and Installation
 
 Requirements:
 
-* C/C++ compiler that supports C++11
+* C/C++ compiler with C++11 support
 * Glib 2.0
 * MPI
 
-
 Navigate into feigns repository and run the following commands:
 
-    cd <feign>
+	FEIGN=<feign-repository-root>
+    cd $FEIGN
     ./waf configure build
 
 Install using:
 
     ./waf install
+
+
+## Plugins
+
+### Using Feign to replay SIOX traces (POSIX)
+
+	cd $FEIGN/tools/gen/layers/posix
+	make export                       # also patches $FEIGN/plugins/wscript
+	cd $FEIGN
+	./waf install
+
+
+
+
--- demos/feign-sample.sh
+++ demos/feign-sample.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-valgrind feign \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample/libfeign_sample.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-provider/libfeign_sample-provider.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-filter/libfeign_sample-filter.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-replayer/libfeign_sample-replayer.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-mutator/libfeign_sample-mutator.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-filter-context/libfeign_sample-filter-context.so \
---plugin $FEIGN_DEV_PLUGIN_PATH/sample-mutator-context/libfeign_sample-mutator-context.so \
+PLUGIN_PATH=$FEIGN_DEV_PLUGIN_PATH/sample
+
+echo $PLUGIN_PATH
+
+feign \
+--plugin $PLUGIN_PATH/sample/libfeign_sample.so \
+--plugin $PLUGIN_PATH/sample-provider/libfeign_sample-provider.so \
+--plugin $PLUGIN_PATH/sample-filter/libfeign_sample-filter.so \
+--plugin $PLUGIN_PATH/sample-replayer/libfeign_sample-replayer.so \
+--plugin $PLUGIN_PATH/sample-mutator/libfeign_sample-mutator.so \
+--plugin $PLUGIN_PATH/sample-filter-context/libfeign_sample-filter-context.so \
+--plugin $PLUGIN_PATH/sample-mutator-context/libfeign_sample-mutator-context.so \
 $@
 
-#--plugin $FEIGN_DEV_PLUGIN_PATH/sample-provider/libfeign_sample-provider2.so \
-#--plugin $FEIGN_DEV_PLUGIN_PATH/sample-filter-context/libfeign_sample-filter-context2.so \
-#--plugin $FEIGN_DEV_PLUGIN_PATH/sample-mutator-context/libfeign_sample-mutator-context2.so \
-#--plugin $FEIGN_DEV_PLUGIN_PATH/sample-replayer/libfeign_sample-replayer2.so \
+#--plugin $PLUGIN_PATH/sample-provider/libfeign_sample-provider2.so \
+#--plugin $PLUGIN_PATH/sample-filter-context/libfeign_sample-filter-context2.so \
+#--plugin $PLUGIN_PATH/sample-mutator-context/libfeign_sample-mutator-context2.so \
+#--plugin $PLUGIN_PATH/sample-replayer/libfeign_sample-replayer2.so \
