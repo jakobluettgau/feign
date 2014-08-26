33c02c8f374920089e5c9d892a864b8cba61356a
Jakob Luettgau <jakob@whatz.eu>
2014-08-26 18:49:24 +0200

    fix demo fadvise name 'typo'

---
+++ demos/siox-fadvise-injection/demo-measures/plot.r
@@ -0,0 +1,11 @@
+library(ggplot2)
+
+# baseline_application.csv
+# baseline_replay.csv
+# fadvise.csv
+
+fad <- read.csv(file="fadvise.csv",head=TRUE,sep=",")
+rep <- read.csv(file="baseline_replay.csv",head=TRUE,sep=",")
+#app <- read.csv(file="baseline_application.csv",head=TRUE,sep=",")
+
+
---
+++ demos/siox-fadvise-injection/measure.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+SAMPLES=3
+
+
+OUT=baseline_application.csv
+for i in {1..3}
+do
+	echo $i
+	./fadvise-readAhead.withoutfadvise.stride12288.think100 >> $OUT
+done
+
+
+
+#OUT=fadvise.csv
+#Echo "us,us/op,op/us" > $OUT
+#For i in {1..3}
+#Do
+#	echo $i
+#	./replay.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
+#Done
+#
+#OUT=baseline_replay.csv
+#Echo "us,us/op,op/us" > $OUT
+#For i in {1..3}
+#Do
+#	echo $i
+#	./replay_unoptimized.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
+#Done
+
