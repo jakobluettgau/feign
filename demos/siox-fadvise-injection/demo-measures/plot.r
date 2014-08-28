673b57f3b16411cddce74dc8f280759ffdf7213f
Jakob Luettgau <jakob@whatz.eu>
2014-08-28 20:11:55 +0200

    better plot

--- demos/siox-fadvise-injection/demo-measures/plot.r
+++ demos/siox-fadvise-injection/demo-measures/plot.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-pdf("plot-fadvise-injection-runtimes.pdf", width=5, height=2)
+pdf("plot-fadvise-injection-runtimes.pdf", width=5, height=4)
 
 # baseline_application.csv
 # baseline_replay.csv
@@ -9,21 +9,23 @@ pdf("plot-fadvise-injection-runtimes.pdf", width=5, height=2)
 opt = read.csv(file="optimized_replay.csv",head=TRUE,sep=",")
 rep = read.csv(file="baseline_replay.csv",head=TRUE,sep=",")
 app <- read.csv(file="baseline_application.csv",head=TRUE,sep=",")
+appopt <- read.csv(file="optimized_application.csv",head=TRUE,sep=",")
 
 optm = mean(opt$us)/1000000
 repm = mean(rep$us)/1000000
 appm = mean(app$runtime)
+appoptm = mean(appopt$runtime)
 
 
-names = c(
-	"Application Baseline",
-	"Replay Baseline",
-	"Optimized Replay"
-	)
-times = c(appm, repm, optm)
+how = c("Application","Application","Replay","Replay")
 
-df = data.frame( run = factor(names, levels=names), runtime = times)
-p = ggplot(data=df, aes(x=run, y=runtime, fill=run)) + geom_bar(stat="identity") + guides(fill=FALSE)
+what = c("Baseline", "Optimized", "Baseline", "Optimized")
+
+times = c(appm, appoptm, repm, optm)
+
+df = data.frame(how=factor(how, levels=c("Application", "Replay")), what=factor(what, levels=c("Baseline", "Optimized")), runtime=times)
+
+p = ggplot(data=df, aes(x=how, y=runtime, fill=what)) + geom_bar(stat="identity", position=position_dodge())
 
 print(p)
 
--- demos/siox-fadvise-injection/measure.sh
+++ demos/siox-fadvise-injection/measure.sh
@@ -3,6 +3,14 @@
 SAMPLES=3
 
 
+OUT=optimized_application.raw
+for i in {1..3}
+do
+	echo $i
+	./fadvise-readAhead.fadvise.stride12288.think100 >> $OUT
+done
+
+
 #OUT=baseline_application.raw
 #for i in {1..3}
 #do
@@ -10,19 +18,19 @@ SAMPLES=3
 #	./fadvise-readAhead.withoutfadvise.stride12288.think100 >> $OUT
 #done
 
-OUT=optimized_replay.csv
-echo "us,us/op,op/us" > $OUT
-for i in {1..3}
-do
-	echo $i
-	./replay.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
-done
-
-OUT=baseline_replay.csv
-echo "us,us/op,op/us" > $OUT
-for i in {1..3}
-do
-	echo $i
-	./replay_unoptimized.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
-done
+#OUT=optimized_replay.csv
+#echo "us,us/op,op/us" > $OUT
+#for i in {1..3}
+#do
+#	echo $i
+#	./replay.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
+#done
+#
+#OUT=baseline_replay.csv
+#echo "us,us/op,op/us" > $OUT
+#for i in {1..3}
+#do
+#	echo $i
+#	./replay_unoptimized.sh demo-trace --loglevel 0 | grep "csv:" | sed -r 's/^.{4}//' >> $OUT
+#done
 
